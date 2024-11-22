d070374f9aefccbc86b285c80fdad4d33b98a0b1
Izan Valverde <24.daw.izanvalverde@fundacionmontessori.com>
2024-11-22 15:55:49 +0100

    Modificacion

--- Ejercicio1 Ramas local UD2/src/saludos/Saludos.java
+++ Ejercicio1 Ramas local UD2/src/saludos/Saludos.java
@@ -1,9 +1,24 @@
 package saludos;
 
-public class Saludos {
+import java.util.Scanner;
 
+public class Saludos {
+	
 	public static void main(String[] args) {
-		  System.out.println("Hola mundo");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Introduce una hora del día: ");
+		int HORA=input.nextInt();
+		if(HORA>0&&HORA<=12) {
+			System.out.println("Buenos días");
+		}else if(HORA>12&&HORA<21) {
+			System.out.println("Buenas tardes");
+		}else if(HORA>=21&&HORA<=24) {
+			System.out.println("Buenas noches");
+		}else {
+			System.out.println("Valor no valido");
+		}
+		
+		input.close();
 
 	}
 
