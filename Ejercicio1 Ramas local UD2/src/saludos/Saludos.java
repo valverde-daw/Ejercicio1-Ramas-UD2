f3a9d7d21d15f91aa8b21f4c20aa76db40139961
Izan Valverde <24.daw.izanvalverde@fundacionmontessori.com>
2024-11-22 15:23:49 +0100

    Modificacion saludos

--- Ejercicio1 Ramas local UD2/src/saludos/Saludos.java
+++ Ejercicio1 Ramas local UD2/src/saludos/Saludos.java
@@ -1,10 +1,17 @@
 package saludos;
 
+import java.util.Scanner;
+
 public class Saludos {
 
 	public static void main(String[] args) {
-		  System.out.println("Hola mundo");
-
+		 Scanner input=new Scanner (System.in);
+		 System.out.print("Introduce tu nombre: ");
+	        String nombre = input.nextLine();
+	        System.out.print("Introduce tus apellidos: ");
+	        String apellidos = input.nextLine();
+	        System.out.println("Hola " + nombre + " " + apellidos);
+		 input.close();
 	}
 
 }
